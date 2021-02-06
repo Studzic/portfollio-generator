f6a584b4b1fd7e99fd3b25f1d55ce14a1faccb85
Austin Dudzic <austin.dudzic@gmail.com>
2021-02-06 13:17:01 -0500

    seperate js

--- app.js
+++ app.js
@@ -1,17 +1,4 @@
-const profileDataArgs = process.argv.slice(2);
-
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 
